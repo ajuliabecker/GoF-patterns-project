6470d3cd0d6e365854b717f28c094f2d8f545870
Júlia Becker de Azevedo <ajuliabecker@gmail.com>
2020-04-22 20:26:23 -0300

    first commit

---
+++ src/main/java/br/com/unisinos/devsoftware/gofpatterns/GofPatternsApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.devsoftware.gofpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unisinos/devsoftware/gofpatterns/GofPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.devsoftware.gofpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
