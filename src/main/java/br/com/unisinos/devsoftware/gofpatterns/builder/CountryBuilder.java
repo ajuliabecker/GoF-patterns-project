cf25182900ec1fa40591b813a5f28c38923aaa7a
Júlia Azevedo <julia.azevedo.ext@conductor.com.br>
2020-09-10 18:51:23 -0300

    remove code smell

--- src/main/java/br/com/unisinos/devsoftware/gofpatterns/builder/CountryBuilder.java
+++ src/main/java/br/com/unisinos/devsoftware/gofpatterns/builder/CountryBuilder.java
@@ -46,18 +46,13 @@ public class CountryBuilder {
 
 
         public Builder(String name) {
-            if (name.isEmpty() && name != null) {
+            if (name.isEmpty()) {
                 throw new IllegalArgumentException("name can not be empty");
             }
             this.name = name;
         }
 
         public Builder deathsPerDay(Integer quantity) {
-            this.deathsPerDay = quantity;
-            return this;
-        }
-
-        public Builder deathsPerDay2(Integer quantity) {
             this.deathsPerDay = quantity;
             return this;
         }
